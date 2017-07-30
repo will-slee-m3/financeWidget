7b480ee286f152cc926cb8bb117b9091ac534c11
mackafat <williamjoshuaslee@outlook.com>
2017-07-30 12:16:54 +0100

    cancel timeouts on component unmounting assetbutton

--- src/client/components/asset-button/asset-button.jsx
+++ src/client/components/asset-button/asset-button.jsx
@@ -19,23 +19,28 @@ export default class AssetButton extends Component {
     super(props);
     this.state = {
       backgroundColor: 'yellow',
+      timeouts: [setTimeout(() => this.setState({ backgroundColor: 'white' }), 300)]
     }
-    setTimeout(() => this.setState({ backgroundColor: 'white' }), 300)
   }
 
   componentWillReceiveProps(nextProps) {
+    const { timeouts } = this.state;
     if (
       this.props.asset.index > nextProps.asset.index
       && nextProps.asset.animate
     ) this.setState({
       backgroundColor: 'green'
-    }, () => setTimeout(() => this.setState({ backgroundColor: 'white' }), 300))
+    }, () => timeouts.push(setTimeout(() => this.setState({ backgroundColor: 'white' }), 300)))
     if (
       this.props.asset.index < nextProps.asset.index
       && nextProps.asset.animate
     ) this.setState({
       backgroundColor: 'red'
-    }, () => setTimeout(() => this.setState({ backgroundColor: 'white' }), 300))
+    }, () => timeouts.push(setTimeout(() => this.setState({ backgroundColor: 'white' }), 300)))
+  }
+
+  componentWillUnmount(){
+    this.state.timeouts.map(t => clearTimeout(t));
   }
 
   render() {
@@ -46,9 +51,6 @@ export default class AssetButton extends Component {
         styles.component,
         { backgroundColor }
       )}>
-        {/* {Object.keys(asset).filter(key => key !== 'top').map(key =>
-          <div>{key}: {asset[key]}</div>
-        )} */}
         <div>{asset.name}</div>
         <div>{asset.price}</div>
         {
